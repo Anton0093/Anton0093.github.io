3d54f4e9c16b1fa72c88deee5db9126b37e461e5
Anton <anton.tamarov0093@gmail.com>
2021-08-16 14:51:37 +0600

    new scroll function

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var findOutMore = document.getElementById('findButt');
+
+var firstCircle = document.getElementById('c1');
+var secondCircle = document.getElementById('c2');
+var thirdCircle = document.getElementById('c3');
+
+var firstPage = document.getElementById('s1');
 var secondPage = document.getElementById('s2');
+var thirdPage = document.getElementById('s3');
+
 var videoBack = document.getElementById("video-overlay");
 var wrapper = document.getElementById("wrapper");
 var popup = document.getElementById("myPopup");
@@ -14,37 +22,51 @@ function scrollTo(element){
 
 // smooth scroll
 findOutMore.addEventListener('click', () => {
-  scrollTo(secondPage);
+  scrollTo(this.secondPage);
+
 })
 
-// video back image none function
-// videoBack.addEventListener('click', () => {
-//   this.videoBack.style.display = 'none'
-// })
+firstCircle.addEventListener('click', () => {
+  this.firstCircle.style.fill = '#7050e5';
 
-// scroll 
-new fullpage('#fullPage', {
-  navigation: true,
-  scrollOverflow: true
+  this.secondCircle.style.fill = '#ff8b00';
+  this.thirdCircle.style.fill = '#ff8b00';
+
+  scrollTo(this.firstPage);
+})
+
+secondCircle.addEventListener('click', () => {
+  this.secondCircle.style.fill = '#7050e5';
+
+  this.firstCircle.style.fill = '#ff8b00';
+  this.thirdCircle.style.fill = '#ff8b00';
+
+  scrollTo(this.secondPage);
+})
+
+thirdCircle.addEventListener('click', () => {
+  this.thirdCircle.style.fill = '#7050e5';
+
+  this.firstCircle.style.fill = '#ff8b00';
+  this.secondCircle.style.fill = '#ff8b00';
+
+  scrollTo(this.thirdPage);
 })
 
 // popup video
 function popupFunc() {
- 
   popup.classList.toggle("show");
-
   wrapper.style.backgroundImage = 'none';
   
   if (popup.paused){ 
       popup.play(); 
       wrapper.style.width = '640px';
       wrapper.style.height = '376px'
-      }
+    }
     else{ 
       popup.pause();
       wrapper.style.width = '500px';
       wrapper.style.height = '300px'
       wrapper.style.backgroundImage = "url('/img/videoBack.png')";
     }
-   
-  }
+}
